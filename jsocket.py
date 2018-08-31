a969c6c877490c65fd27827417220d82d49ac416
Shantanu Vyas <shenty@localhost.localdomain>
2018-08-31 17:01:41 -0400

    updates

--- jsocket.py
+++ jsocket.py
@@ -6,135 +6,141 @@ import struct
 # Python 3
 #class JsonSocket(abc.ABC)
 class JsonSocket:
-	__metaclass__ = abc.ABCMeta
+        __metaclass__ = abc.ABCMeta
 
-	def __init__(self, sock=None):
-		if sock is None:
-			self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		else:
-			self._socket = sock
+        def __init__(self, sock=None,udp=None):
+                if sock is None:
+                        if udp == None or udp == False:
+                                self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                                print('tcp')
+                        elif udp == True:
+                                self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                                print('udp')
 
-		#self._conn = self._socket
+                else:
+                        self._socket = sock
 
-		# Must be 2 right now because struct.pack/unpack are hardcoded below
-		self._header_size = 2
+                #self._conn = self._socket
 
-	def __enter__(self):
-		return self
+                # Must be 2 right now because struct.pack/unpack are hardcoded below
+                self._header_size = 2
 
-	def __exit__(self, type, value, traceback):
-		if isinstance(value, RuntimeError):
-			print("Socket connection broken, closing socket...")
-			self.close()
-			return True
+        def __enter__(self):
+                return self
 
-	def _send(self, msg):
-		sent = 0
-		while sent < len(msg):
-			sent += self._socket.send(msg[sent:])
+        def __exit__(self, type, value, traceback):
+                if isinstance(value, RuntimeError):
+                        print("Socket connection broken, closing socket...")
+                        self.close()
+                        return True
 
-	def _read(self, size):
-		data = bytearray()
-		while len(data) < size:
-			new_data = self._socket.recv(size - len(data))
-			print(new_data)
-			data.extend(new_data)
-			if new_data == b'':
-				#print("Socket connection broken, handle appropriately")
-				raise RuntimeError("Socket connection broken")
+        def _send(self, msg):
+                sent = 0
+                while sent < len(msg):
+                        sent += self._socket.send(msg[sent:])
 
-		return data
+        def _read(self, size):
+                data = bytearray()
+                while len(data) < size:
+                        new_data = self._socket.recv(size - len(data))
+                        #print(new_data)
+                        data.extend(new_data)
+                        if new_data == b'':
+                                #print("Socket connection broken, handle appropriately")
+                                raise RuntimeError("Socket connection broken")
 
-	def _msg_length(self):
-		header = self._read(self._header_size)
-		length = struct.unpack('!H', header)[0]
-		#int.from_bytes(header, byteorder='big')
+                return data
 
-		return length
+        def _msg_length(self):
+                header = self._read(self._header_size)
+                length = struct.unpack('!H', header)[0]
+                #int.from_bytes(header, byteorder='big')
+
+                return length
 
 
-	def send_obj(self, obj, encode_func=json.dumps):
-		msg = encode_func(obj)
+        def send_obj(self, obj, encode_func=json.dumps):
+                msg = encode_func(obj)
 
-		if self._socket:
-			print(len(msg), len(msg.encode('utf-8')))
-			# Python 3
-			#format_str = f"={len(msg)}s"
-			format_str = "={0}s".format(len(msg))
-			msg_packed = struct.pack(format_str, msg.encode('utf-8'))
-			msg_length = len(msg_packed)
-			header_packed = struct.pack('!H', msg_length)
-			#msg_length.to_bytes(self._header_size, byteorder='big')
+                if self._socket:
+                        print(len(msg), len(msg.encode('utf-8')))
+                        # Python 3
+                        #format_str = f"={len(msg)}s"
+                        format_str = "={0}s".format(len(msg))
+                        msg_packed = struct.pack(format_str, msg.encode('utf-8'))
+                        msg_length = len(msg_packed)
+                        header_packed = struct.pack('!H', msg_length)
+                        #msg_length.to_bytes(self._header_size, byteorder='big')
 
-			print("Sending packed header {0}".format(header_packed))
-			self._send(header_packed)
-			print("Sending packed message {0}".format(msg_packed))
-			self._send(msg_packed)
+                        print("Sending packed header {0}".format(header_packed))
+                        self._send(header_packed)
+                        print("Sending packed message {0}".format(msg_packed))
+                        self._send(msg_packed)
 
-	def read_obj(self, decode_func=json.loads):
-		size = self._msg_length()
-		data = self._read(size)
-		# Python 3
-		#format_str = f"={size}s"
-		format_str = "={0}s".format(size)
-		msg = struct.unpack(format_str, data)[0]
+        def read_obj(self, decode_func=json.loads):
+                size = self._msg_length()
+                data = self._read(size)
+                # Python 3
+                #format_str = f"={size}s"
+                format_str = "={0}s".format(size)
+                msg = struct.unpack(format_str, data)[0]
 
-		return decode_func(msg)
+                return decode_func(msg)
 
-	def close(self):
-		self._socket.close()
+        def close(self):
+                self._socket.close()
 
-	@property
-	def socket(self):
-		return self._socket
+        @property
+        def socket(self):
+                return self._socket
 
-	@socket.setter
-	def socket(self, new_socket):
-		self._socket = new_socket
+        @socket.setter
+        def socket(self, new_socket):
+                self._socket = new_socket
 
 
 class JsonServer(JsonSocket):
 
-	def __init__(self, address=None, port=None):
-		# Python 3
-		#super().__init__()
-		super(JsonServer, self).__init__()
+        def __init__(self, address=None, port=None, udp=False):
+                # Python 3
+                #super().__init__()
+                super(JsonServer, self).__init__()
 
-		self._address = address
-		self._port = port
+                self._address = address
+                self._port = port
 
 
-	def bind(self, address, port):
-		if address is None:
-			address = socket.gethostname()
+        def bind(self, address, port):
+                if address is None:
+                        address = socket.gethostname()
 
-		print("binding to {0}".format(address))
-		self.socket.bind((address, port))
-		self._address = address
-		self._port = port
+                print("binding to {0}".format(address))
+                self.socket.bind((address, port))
+                self._address = address
+                self._port = port
 
-	def listen(self):
-		self.socket.listen(0)
+        def listen(self):
+                self.socket.listen(0)
 
-	def accept(self):
-		conn, addr = self._socket.accept()
-		#conn.settimeout(2.0)
+        def accept(self):
+                conn, addr = self._socket.accept()
+                #conn.settimeout(2.0)
 
-		return JsonSocket(conn)
+                return JsonSocket(conn)
 
 
 class JsonClient(JsonSocket):
+        def __init__(self, address=None, port=None,_udp=None):
+                # Python 3
+                #super().__init__()
 
-	def __init__(self, address=None, port=None):
-		# Python 3
-		#super().__init__()
-		super(JsonClient, self).__init__()
+                super(JsonClient, self).__init__(None,_udp)
 
-		self._address = address
-		self._port = port
+                self._address = address
+                self._port = port
 
-	def connect(self, address, port):
-		self.socket.connect((address, port))
+        def connect(self, address, port):
+                self.socket.connect((address, port))
 
-		self._address = address
-		self._port = port
+                self._address = address
+                self._port = port
--- telemetry.py
+++ telemetry.py
@@ -1,7 +1,7 @@
 from messages import Message, MessageEncoder
 from jsocket import JsonClient
 import pymavlink.mavutil as mavutil
-import threading 
+import threading
 import yaml
 import Queue
 import json
@@ -13,7 +13,7 @@ class TelemetrySender(object):
 
     def __init__(self,ipaddr):
         self.server_ip = ipaddr
-    
+
     def init_message_dispatch(self):
         message_queue = Queue.Queue()
         msgd_t = threading.Thread(target=self.message_dispatch,args=(message_queue,))
@@ -23,30 +23,27 @@ class TelemetrySender(object):
 
     def message_dispatch(self,message_queue):
         print 'attempting to connect to ', self.server_ip
-        jsock = JsonClient()
+        jsock = JsonClient(_udp=True)
         jsock.connect(self.server_ip, 6780)
         print 'connected to ', self.server_ip
 
         while True:
             if message_queue.empty():
                 pass
-            raw_msg = message_queue.get()
+
             msg = str(message_queue.get())
 
             tag = msg.partition(' ')[0]
             payload = msg.split(' ', 1)[1]
 
-            # print 'type:', tag, '\npayload:',payload
-            # print 'type', type(yaml.load(payload))
-            
             json_payload =  yaml.load(payload) #converts names into strings from raw values
 
             net_msg = Message(tag,json_payload)
             jsock.send_obj(net_msg, lambda obj: json.dumps(obj, cls=net_msg.json_encoder, indent=2))
-        
+
     def get_messages(self,message_queue):
     #have to make sure msg is a string for this to work
-        mav = mavutil.mavlink_connection('tcp:127.0.0.1:5760')
+        mav = mavutil.mavlink_connection('udp:127.0.0.1:5760')
         mav.wait_heartbeat()
 
         while True:
@@ -54,9 +51,10 @@ class TelemetrySender(object):
             if msg is None:
                 pass
             message_queue.put(msg)
-            
+
 
 
 #init_message_dispatch()
-t = TelemetrySender('192.168.0.132')
+#t = TelemetrySender('192.168.0.132')
+t = TelemetrySender('127.0.0.1')
 t.init_message_dispatch()
